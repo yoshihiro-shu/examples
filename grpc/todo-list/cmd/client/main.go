5f689f90494e5a148313bf4c824ab47a27a6ee8e
yoshihiro-shu <yoihongzhu0321@gmail.com>
2023-09-17 18:02:01 +0900

    add: proto client

--- grpc/todo-list/cmd/client/main.go
+++ grpc/todo-list/cmd/client/main.go
@@ -1,5 +1,47 @@
 package main
 
-func main() {
+import (
+	"context"
+	"flag"
+	"log"
+	"time"
 
+	pb "github.com/yoshihiro-shu/examples/grpc/todo-list/todo"
+	"google.golang.org/grpc"
+	"google.golang.org/grpc/credentials/insecure"
+)
+
+const (
+	defaultName = "todo"
+)
+
+var (
+	addr = flag.String("addr", "localhost:50051", "the address to connect to")
+	name = flag.String("name", defaultName, "todo client")
+)
+
+func main() {
+	flag.Parse()
+	// Set up a connection to the server.
+	conn, err := grpc.Dial(*addr, grpc.WithTransportCredentials(insecure.NewCredentials()))
+	if err != nil {
+		log.Fatalf("did not connect: %v", err)
+	}
+	defer conn.Close()
+	c := pb.NewTodoServiceClient(conn)
+
+	// Contact the server and print out its response.
+	ctx, cancel := context.WithTimeout(context.Background(), time.Second)
+	defer cancel()
+	// r, err := c.SayHello(ctx, &pb.HelloRequest{Name: *name})
+	r, err := c.CreateTask(ctx, &pb.CreateTaskRequest{
+		Title:       "test title 1",
+		Description: "test descrption 1",
+	})
+	if err != nil {
+		log.Fatalf("could not greet: %v", err)
+	}
+	t := r.GetTask()
+	log.Printf("task: %s", t.Title)
+	log.Printf("task: %s", t.Description)
 }
