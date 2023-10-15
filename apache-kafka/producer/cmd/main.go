05cbdfe5718c6555165f34be8f372d17fcf4d84b
yoshihiro-shu <yoihongzhu0321@gmail.com>
2023-10-15 16:34:05 +0900

    add: appache kafka

---
+++ apache-kafka/producer/cmd/main.go
@@ -0,0 +1,58 @@
+package main
+
+import (
+	"log"
+
+	"github.com/IBM/sarama"
+)
+
+type Producer struct {
+	sarama.SyncProducer
+}
+
+func NewProducer() (*Producer, error) {
+	config := sarama.NewConfig()
+	config.Producer.Return.Successes = true
+	config.Producer.Return.Errors = true
+
+	producer, err := sarama.NewSyncProducer([]string{"localhost:9092"}, config)
+	if err != nil {
+		log.Fatalf("Failed to create producer: %s", err)
+		return nil, err
+	}
+
+	return &Producer{producer}, nil
+}
+
+func (p *Producer) SendMessages(topic string, key, value []byte) error {
+	msg := &sarama.ProducerMessage{
+		Topic: topic,
+		Key:   sarama.StringEncoder(key),
+		Value: sarama.StringEncoder(value),
+	}
+	_, _, err := p.SyncProducer.SendMessage(msg)
+	if err != nil {
+		return err
+	}
+	return nil
+}
+
+func main() {
+	producer, err := NewProducer()
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer producer.Close()
+
+	log.Printf("==> start producing %#v", producer)
+	log.Printf("==> status of producing %#v", producer.TxnStatus())
+
+	topic := "news"
+	key := []byte("key")
+	value := []byte("value")
+	err = producer.SendMessages(topic, key, value)
+	if err != nil {
+		log.Fatal(err)
+	}
+	log.Println("success")
+}
